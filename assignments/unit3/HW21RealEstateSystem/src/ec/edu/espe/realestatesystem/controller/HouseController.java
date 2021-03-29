d33489786ab596ff81fc8c106239e198061e12fc
evelynquilca <evelyn2001mquilca@gmail.com>
2021-03-28 22:15:05 -0500

    unit3

---
+++ assignments/unit3/HW21RealEstateSystem/src/ec/edu/espe/realestatesystem/controller/HouseController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.realestatesystem.controller;
+
+import ec.edu.espe.realestatesystem.model.House;
+import utils.FileManager;
+
+/**
+ *
+ * @author hp
+ */
+public class HouseController {
+    public void save(House house){
+        
+        String data = house.getColor() + ", " + house.getForm() + ", " + house.getAge() + ", " + house.getSize()+ ", " + house.getPrince();
+        FileManager.save(data, "houses");
+    }
+    
+    public String read(){
+        String data;
+        data = FileManager.read("houses");
+        return data;
+    }
+    
+}
+    
